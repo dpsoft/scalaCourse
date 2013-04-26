3516d0e661e31ebed5c0ca661d1c5b1b2cb27f23
Diego Parra <diegolparra@gmail.com>
2013-04-25 22:10:51 -0300

    refactor controller

--- play/app/controllers/RestController.scala
+++ play/app/controllers/RestController.scala
@@ -2,6 +2,7 @@ package controllers
  
 import play.api.mvc.{Action, Controller}
 
+import play.api.libs.functional.syntax._
 import play.api.libs.json._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.ws.WS
@@ -10,13 +11,20 @@ case class Result(from: String, text: String)
  
 object RestController extends Controller {
 
+  implicit val tweetReads = (
+    (__ \ "from_user_name").read[String] and
+      (__ \ "text").read[String]
+    )(Result)
+
+  implicit val tweetsWrites = Json.writes[Result]
+
   def call(query: String) = Action {
     Async {
-      val responsePromise =  WS.url("http://search.twitter.com/search.json").get
+      val responsePromise =  WS.url("http://search.twitter.com/search.json").withQueryString(("q", query)).get
       responsePromise.map {
         response =>
           val results = Json.parse(response.body).\("results").as[Seq[Result]]
-          Ok(results)
+          Ok(Json.toJson(results.map(r => Json.toJson(r))))
       }
     }
   }
